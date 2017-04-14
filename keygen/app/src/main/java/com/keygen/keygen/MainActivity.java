3b12cdf208a74da0afeee68237abcfd0d2091c25
reed-j <reed4j@gmail.com>
2017-04-14 15:33:12 +0300

    auth

--- keygen/app/src/main/java/com/keygen/keygen/MainActivity.java
+++ keygen/app/src/main/java/com/keygen/keygen/MainActivity.java
@@ -2,6 +2,8 @@ package com.keygen.keygen;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void onClick(View view) {
+        TextView helloTextView = (TextView)findViewById(R.id.textView);
+        helloTextView.setText("Hello Kitty!");
     }
 }
